782e0082e0dd7b98b25df5fe9b8b78cf1ac258dd
Giyoul <cla12093@gmail.com>
2025-10-19 17:08:36 +0900

    test: add empty input test

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,15 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 빈_입력_테스트() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
