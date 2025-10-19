506d6bc78d4cffac1a5334c5e3fffdf668ce6879
Giyoul <cla12093@gmail.com>
2025-10-19 18:34:00 +0900

    docs: add application java docs

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,17 @@ import calculator.service.CalculatorService;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
+/**
+ * Main class of the calculator application.
+ * Gets user input, performs calculations, and outputs results.
+ */
 public class Application {
+
+    /**
+     * Entry point of the application.
+     *
+     * @param args command line
+     */
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
