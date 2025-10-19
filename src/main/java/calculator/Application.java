387d556195e3ae58d087fb9e6b4e7e600b0a5f23
Giyoul <cla12093@gmail.com>
2025-10-19 15:30:32 +0900

    refactor(console): extract I/O handling to separate class
    Move displying start message and result feature to OutputView
    Move Console.readLine() logic and Console.close() logic to InputView
    Remove Application.java I/O logic code

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,72 +1,18 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        String output = "결과 : ";
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CalculatorService calculatorService = new CalculatorService();
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView, calculatorService);
 
-        List<Character> delimiters = new ArrayList<>(List.of(',', ':'));
-        Pattern customDelimiterPattern = Pattern.compile("^//(.+)\\\\n"); // 문자열의 시작만 확인
-        Matcher customDelimiter = customDelimiterPattern.matcher(input);
-
-        if(customDelimiter.find()){
-            String delimiter = customDelimiter.group(1);
-            if (delimiter.equals(",") || delimiter.equals(":")) {
-                throw new IllegalArgumentException("Invalid custom delimiter: ',' and ':' are not allowed");
-            }
-            if (delimiter.length() > 1) {
-                throw new IllegalArgumentException(
-                        "Invalid custom delimiter: Delimiter length must be less than or equal to 1");
-            }
-            delimiters.add(delimiter.charAt(0));
-        }
-
-        String remaining = customDelimiter.replaceFirst("");
-        String numSeparateDelimiterPattern = delimiters.stream()
-                .map(d -> "\\" + d)
-                .collect(Collectors.joining("|"));
-
-        delimiters.stream().forEach(d -> {
-            String delim = String.valueOf(d);
-            if (remaining.startsWith(delim)) {
-                throw new IllegalArgumentException("Format error: Input cannot start with delimiter.");
-            }
-            if (remaining.endsWith(delim)) {
-                throw new IllegalArgumentException("Format error: Input cannot end with delimiter.");
-            }
-        });
-
-        String[] nums = remaining.split(numSeparateDelimiterPattern);
-        nums = Arrays.stream(nums)
-                .filter(s -> !s.isEmpty())
-                .toArray(String[]::new);
-
-        int ans = Arrays.stream(nums)
-                .mapToInt(s -> {
-                    try {
-                        int num = Integer.parseInt(s);
-                        if (num < 0) {
-                            throw new IllegalArgumentException("Invalid argument: Negative numbers are not allowed");
-                        }
-                        return num;
-                    } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException("Invalid argument: Include invalid character");
-                    }
-                })
-                .sum();
-        output += String.valueOf(ans);
-
-        System.out.println(output);
-        Console.close();
+        calculatorController.run();
     }
 }
