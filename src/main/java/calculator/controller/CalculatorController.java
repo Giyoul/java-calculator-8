29a7dcf48e6e12cfa9a5bc461f61a09c72c2fc9b
Giyoul <cla12093@gmail.com>
2025-10-19 15:32:17 +0900

    refactor(domain): add CalculatorController for flow
    Create controller for application flow
    Implement DI pattern
    Separate between View and Service layers

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,2 +1,34 @@
-package calculator.controller;public class CalculatorController {
+package calculator.controller;
+
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(InputView inputView, OutputView outputView, CalculatorService calculatorService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculatorService = calculatorService;
+    }
+
+    public void run() {
+        // 1. Display start massage
+        outputView.printStartMessage();
+
+        // 2. Get users input
+        String input = inputView.readInput();
+
+        // 3. Calculate result
+        String result = calculatorService.calculate(input);
+
+        // 4. Display result
+        outputView.printResultMessage(result);
+
+        // 5. Close input console
+        inputView.closeConsole();
+    }
 }
