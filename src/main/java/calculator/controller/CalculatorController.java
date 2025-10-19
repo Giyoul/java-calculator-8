92228fe5f3e035696aa4887916ce86a01e151379
Giyoul <cla12093@gmail.com>
2025-10-19 18:43:28 +0900

    docs: Add CalculatorController java docs

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -4,17 +4,30 @@ import calculator.service.CalculatorService;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
+/**
+ * Controller class that manages the flow of the calculator application.
+ */
 public class CalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
     private final CalculatorService calculatorService;
 
+    /**
+     * Constructor of CalculatorController
+     *
+     * @param inputView input handling view
+     * @param outputView output handling view
+     * @param calculatorService calculation service
+     */
     public CalculatorController(InputView inputView, OutputView outputView, CalculatorService calculatorService) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.calculatorService = calculatorService;
     }
 
+    /**
+     * Runs the calculator application
+     */
     public void run() {
         // 1. Display start massage
         outputView.printStartMessage();
