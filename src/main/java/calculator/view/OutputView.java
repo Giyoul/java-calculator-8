b6f179287c86c274a933472523d54f5d39e1475b
Giyoul <cla12093@gmail.com>
2025-10-19 18:45:18 +0900

    docs(console): Add OutputView java docs

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,10 +1,22 @@
 package calculator.view;
 
+/**
+ * View class that handles output operations.
+ */
 public class OutputView {
+
+    /**
+     * Prints the start message to the console.
+     */
     public void printStartMessage() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
+    /**
+     * Prints the calculation result to the console.
+     *
+     * @param result the result value to print
+     */
     public void printResultMessage(String result) {
         System.out.println(result);
     }
