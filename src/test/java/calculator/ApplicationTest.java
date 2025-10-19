67d4cc519b951fba51532a13bcca5cd6ab15c3ca
Giyoul <cla12093@gmail.com>
2025-10-19 17:16:19 +0900

    test: add exception test for negative number input

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -80,6 +80,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 음수_입력_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
